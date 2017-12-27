d5c739bb1fd2d6895014deae731b1c55850e2735
Jean-Marie Dubille <jmd@bedule-conseil.fr>
2017-12-27 23:18:38 +0100

    Initiate module rancher-cli

---
+++ modules/rancher-cli/_rancher-cli.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2017, bedule conseil
+# Copyright: (c) 2017, Jean-Marie Dubille <jmd@bedule-conseil.Fr>
+# Gnu General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['deprecated'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: rancher-cli
+version_added: "2.4"
+short_description: manage rancher catalog config environments stacks and services
+deprecated:
+description:
+  - Ansible module for managing docker container life cycle in a rancher environment
+'''
+
